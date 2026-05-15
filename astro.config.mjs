04a7baca269873d38063e698d22a0cedb442fb78
theclstrickland <connor@redemptivedigital.com>
2026-05-14 22:24:25 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://fayettevilleseptictank.com',
+  integrations: [sitemap()],
   build: {
     format: 'directory',
   },
